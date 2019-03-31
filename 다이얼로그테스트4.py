a8bf88446cb1e123e702e155f7fda8632b92f283
reharmony <44914115+reharmony@users.noreply.github.com>
2019-03-31 21:37:56 +0900

    미니 계산기 수정

--- 다이얼로그테스트4.py
+++ 다이얼로그테스트4.py
@@ -12,62 +12,65 @@ def onClick1():
     data1 = int(num1_in.get())
     data2 = int(num2_in.get())
     result = data1 + data2
-    end["text"] = "계산결과는" + str(result)
+    end["text"] = "계산결과는 " + str(result)
 
 def onClick2():
     data1 = int(num1_in.get())
     data2 = int(num2_in.get())
     result = data1 - data2
-    end["text"] = "계산결과는" + str(result)
+    end["text"] = "계산결과는 " + str(result)
 
 def onClick3():
     data1 = int(num1_in.get())
     data2 = int(num2_in.get())
     result = data1 * data2
-    end["text"] = "계산결과는" + str(result)
+    end["text"] = "계산결과는 " + str(result)
     
 def onClick4():
     data1 = int(num1_in.get())
     data2 = int(num2_in.get())
     result = data1 / data2
-    end["text"] = "계산결과는" + str(result)
+    end["text"] = "계산결과는 " + str(result)
     
 w = Tk()
 w.title("미니 계산기")
-w.geometry("240x200-900-500")
-
+w.geometry("240x150-900-500")
+f1=Frame(w)
+f1.pack(side="top")
+f2=Frame(w)
+f2.pack(side="bottom")
 
 # 숫자 입력1
-num1 = Label(w, text = "첫번째 숫자를 입력하세요.")
+num1 = Label(f1, text = "첫번째 숫자를 입력하세요.")
 num1.pack()
 
-num1_in = Entry(w)
+num1_in = Entry(f1)
 num1_in.pack()
 
 # 숫자 입력2
-num2 = Label(w, text = "두번째 숫자를 입력하세요.")
+num2 = Label(f1, text = "두번째 숫자를 입력하세요.")
 num2.pack()
 
-num2_in = Entry(w)
+num2_in = Entry(f1)
 num2_in.pack()
 
 # 버튼 4개 만들기
-plus = Button(w, text ="+", command = onClick1, width = 5)
-plus.pack(side = "left", padx=7, pady=1)
+plus = Button(f1, text ="+", command = onClick1, width = 5)
+plus.pack(side = "left", padx=7,pady=10)
 
-minus = Button(w, text ="-", command = onClick2, width = 5)
-minus.pack(side = "left", padx=7, pady=1)
+minus = Button(f1, text ="-", command = onClick2, width = 5)
+minus.pack(side = "left", padx=7)
 
-mul = Button(w, text ="*", command = onClick3, width = 5)
-mul.pack(side = "left", padx=7, pady=1)
+mul = Button(f1, text ="*", command = onClick3, width = 5)
+mul.pack(side = "left", padx=7)
 
-div = Button(w, text ="/", command = onClick4, width = 5)
-div.pack(side = "left", padx=7, pady=1)
+div = Button(f1, text ="/", command = onClick4, width = 5)
+div.pack(side = "left", padx=7)
 
 # 결과 출력창
-f2=Frame()
-end = Label(w, text = "여기에 계산 결과 출력")
-end.pack(padx=7, pady=100)
+
+end = Label(f2, text = "여기에 계산 결과 출력")
+end.pack(pady=5)
 
 w = mainloop()
 
