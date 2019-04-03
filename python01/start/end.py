6fffea130e8c88d408fd6ac31ba05be7fdb8fcd4
reharmony <44914115+reharmony@users.noreply.github.com>
2019-04-03 15:46:04 +0900

    Add files via upload

---
+++ python01/start/end.py
@@ -0,0 +1,11 @@
+dept = input("부서: ")
+subject = input("과목: ")
+old = input("지난번 성적: ")
+new = input("이번 성적: ")
+
+a = int(old)
+b = int(new)
+print("부서:", dept)
+print("과목:", subject)
+print(a + b)
+
