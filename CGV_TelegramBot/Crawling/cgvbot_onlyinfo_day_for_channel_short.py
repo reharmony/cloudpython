4a52fd37c72353b96e5be0e3a3d40d8a94797251
reharmony <44914115+reharmony@users.noreply.github.com>
2019-04-19 02:06:40 +0900

    Add files via upload

---
+++ CGV_TelegramBot/Crawling/cgvbot_onlyinfo_day_for_channel_short.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import datetime
+import os
+import io
+from bs4 import BeautifulSoup  
+import telepot
+import time
+import threading
+
+# 목표날짜
+target_day = "20190424"
+
+url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=%s' % target_day 
+response = requests.get(url) 
+html = response.text
+# BS 이용해서 원하는 값만 추출
+soup = BeautifulSoup(html, 'html.parser') #html.parser를 사용해서 soup에 넣겠다
+
+showing=""
+time_str=""
+
+token= "836234072:AAEKFOj3hmfbhg-pY5ScDrbxPUiYCG39xe8"
+
+mc = "-1001159590408"
+
+bot = telepot.Bot(token)
+ 
+status = True    
+   
+for a in range (0,20):
+    for tag in soup.select('li:nth-child(%d) > div > div > div.info-hall > ul > li:nth-child(2) > span > span' % a):                  
+        screen = tag.text.strip()
+        if screen == 'IMAX':
+            # 제목 찾기
+            for tag in soup.select('li:nth-child(%d)> div > div.info-movie > a > strong' % a):
+                title = tag.text.strip()
+                # 테이블 안에서 아맥관 순서 찾기
+                for b in range(0,20):
+                    for tag in soup.select('li:nth-child(%d) > div > div:nth-child(%d) > div.info-hall > ul > li:nth-child(2) > span > span' % (a, b)):        
+                        screen = tag.text.strip()
+                        if screen == 'IMAX':
+                            time_str=""
+                            # 상영시간 및 잔여좌석 찾기                                                                
+                            for tag in soup.select('li:nth-child(%d) > div > div:nth-child(%d) > div.info-timetable > ul > li > a' % (a,b)):                            
+                                taglist= tag.text.strip()
+                                showtime = taglist.replace(u"잔여좌석", u"잔여좌석: ")
+                                time_str += "\n" + showtime
+                            if time_str == "":
+                                pass
+                            else:                          
+                                bot.sendMessage(mc,time_str)
+                                print("메세지 전송 완료") 
+
+
+    
+    
+
